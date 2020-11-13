6401192706fddb298e3cc5bc353c0a84d6a3580a
crayon18 <tnvudtjs3355@naver.com>
2020-11-14 00:31:00 +0900

    abstract class test

---
+++ src/Computer.java
@@ -0,0 +1,15 @@
+
+public abstract class Computer {
+
+	public abstract void display();
+	public abstract void typing();
+	
+
+	public void turnOn() {
+		System.out.println("Àü¿øÄÔ");
+	}
+	
+	public void turnOff() {
+		System.out.println("Àü¿ø²û"); 
+	}
+}
