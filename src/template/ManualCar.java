90bd69c75d0b795f22461a951bd7610b32373526
crayon18 <tnvudtjs3355@naver.com>
2020-11-14 23:36:02 +0900

    abstract class test

---
+++ src/template/ManualCar.java
@@ -0,0 +1,20 @@
+package template;
+
+public class ManualCar extends Car {
+
+	public void drive() {
+		System.out.println("사람이 운전합니다");
+		
+	}
+
+	public void stop() {
+		System.out.println("사람이 멈춥니다");
+		
+	}
+
+	public void wiper() {
+		System.out.println("비나 눈의 양에 따라 사람이 스스로 조절합니다");	 
+		
+	}
+
+}
